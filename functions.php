249702522d4574780a289fafe706072cacf8ccf3
m-e-h <info@martyhelmick.com>
2015-01-20 14:37:03 -0500

    add base files

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package    Trailing Spaces
+ * @author     Marty Helmick <info@martyhelmick.com>
+ * @copyright  Copyright (c) 2014, Marty Helmick
+ * @link       https://github.com/m-e-h/trailing-spaces
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+/* Add the child theme setup function to the 'after_setup_theme' hook. */
+add_action( 'after_setup_theme', 'doc_child_theme_setup' );
+
+/**
+ * Setup function.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+function doc_child_theme_setup() {
+}
