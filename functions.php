2756fd093b4c4dfa305d8d02410bf420fe9f4236
m-e-h <info@martyhelmick.com>
2015-05-08 19:00:47 -0400

    move panel to parent

--- functions.php
+++ functions.php
@@ -6,7 +6,6 @@
  */
 
 add_action( 'after_setup_theme', 'doc_theme_setup' );
-add_action( 'widgets_init', 'doc_register_sidebars' );
 
 
 
@@ -17,37 +16,11 @@ add_action( 'widgets_init', 'doc_register_sidebars' );
  */
 function doc_theme_setup() {
 
-	// Register menu for the dropdown panel.
-    register_nav_menus( [
-        'panel-dpc' => _x( 'Pastoral Center Panel Menu', 'doc' ),
-    ] );
+
 
 }
 
 
-
-
-/**
- * Register Panel Widgets.
- */
-function doc_register_sidebars() {
-	hybrid_register_sidebar( array(
-        'id'            => 'panel-parishes',
-        'name'          => _x( 'Parish Panel Widgets', 'doc' ),
-		'before_widget' => '<section id="%1$s" class="widget widget-panel %2$s u-pr- u-pr@md u-pr+@lg u-mb- u-mb@md u-mb+@lg grid__item grid__item--flexed"><div class="br widget__wrap u-p@all t-bg__1--glass shadow--z1">',
-		'after_widget'	=> '</div></section>',
-		'before_title'  => '<h3 class="widget-title widget-panel__title">',
-		'after_title'	=> '</h3>',
-    ) );
-	hybrid_register_sidebar( array(
-        'id'            => 'panel-schools',
-        'name'          => _x( 'School Panel Widgets', 'doc' ),
-		'before_widget' => '<section id="%1$s" class="widget widget-panel %2$s u-pr- u-pr@md u-pr+@lg u-mb- u-mb@md u-mb+@lg grid__item grid__item--flexed"><div class="br widget__wrap u-p@all t-bg__1--glass shadow--z1">',
-		'after_widget'	=> '</div></section>',
-		'before_title'  => '<h3 class="widget-title widget-panel__title">',
-		'after_title'	=> '</h3>',
-    ) );
-}
 
 
 
